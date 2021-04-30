eb690c48cbe5a5a7e5e3060d99bbf09b399d98a8
GiuseppeTT <giu.tinti@gmail.com>
2021-04-30 22:37:09 +0000

    Add sImple CLI

--- ratinhonet.py
+++ ratinhonet.py
@@ -1,36 +1,68 @@
+from typing import Optional
+
 import numpy as np
 import sounddevice as sd
 import soundfile as sf
+import typer
 
-CHANNELS = 1
-LOG = True
-COOLDOWN = 500
-UPDATE_WEIGHT = 0.03
-CUT_OFF = 10
+app = typer.Typer()
 
-countdown = COOLDOWN
+countdown = 0
 old_mean = 0
-ratinho_sound, ratinho_sample_rate = sf.read("audio/fart.wav")
 
 
-def callback(amplitudes, frames, time, status):
+@app.command()
+def run(
+    source: Optional[str] = typer.Argument(
+        None,
+        help="Audios source path. If none, use built-in",
+    ),
+    cooldown: int = typer.Option(
+        1_000,
+        help="Wait at least --cooldown seconds before emiting another sound",
+    ),
+    cut_off: float = typer.Option(
+        10,
+        help="How much INsensible RatinhoNet should be to sudden volume changes",
+    ),
+    log: bool = typer.Option(False),
+):
+    """
+    Run RatinhoNet with SOURCE audios.
+
+    Audios should be short (< 5 seconds) WAV files.
+    """
+
+    update_weight = 0.03
+    channels = 1
+
     global countdown, old_mean
 
-    volumes = np.abs(amplitudes)
-    current_mean = np.mean(volumes)
-    mean_ratio = current_mean / old_mean
+    countdown = cooldown
+    old_mean = 0
+    ratinho_sound, ratinho_sample_rate = sf.read("audio/fart.wav")
 
-    if LOG:
-        print(f"countdown: {countdown}, mean ratio: {mean_ratio:.2f}")
+    def callback(amplitudes, frames, time, status):
+        global countdown, old_mean
 
-    if countdown == 0 and mean_ratio > CUT_OFF:
-        countdown = COOLDOWN
-        sd.play(ratinho_sound, ratinho_sample_rate)
+        volumes = np.abs(amplitudes)
+        current_mean = np.mean(volumes)
+        mean_ratio = current_mean / old_mean
 
-    countdown -= 1
-    countdown = 0 if countdown < 0 else countdown
-    old_mean = (1 - UPDATE_WEIGHT) * old_mean + UPDATE_WEIGHT * current_mean
+        if log:
+            typer.echo(f"countdown: {countdown}, mean ratio: {mean_ratio:.2f}\r", nl=False)
+
+        if countdown == 0 and mean_ratio > cut_off:
+            countdown = cooldown
+            sd.play(ratinho_sound, ratinho_sample_rate)
+
+        countdown -= 1
+        countdown = 0 if countdown < 0 else countdown
+        old_mean = (1 - update_weight) * old_mean + update_weight * current_mean
+
+    with sd.InputStream(channels=channels, callback=callback):
+        input()
 
 
-with sd.InputStream(channels=CHANNELS, callback=callback):
-    input()
+if __name__ == "__main__":
+    app()
